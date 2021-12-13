342d84113bcfb8e61fcdceb414a4cdc4fd7d251f
machuang <machuang@biaoguoworks.com>
2021-12-13 14:33:28 +0800

    update zozo-theme

--- push.sh
+++ push.sh
@@ -2,6 +2,6 @@ git add .
 git commit -m "update zozo-theme"
 git push
 hugo
-docker build --platform linux/amd64 -t mabutou/hugo-blog .
-docker push mabutou/hugo-blog
+# docker build --platform linux/amd64 -t mabutou/hugo-blog .
+# docker push mabutou/hugo-blog
 # wrangler publish
