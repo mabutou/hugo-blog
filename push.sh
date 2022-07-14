0c6a9cee38bf764aa402b698fce920d843025208
machuang <machuang@biaoguoworks.com>
2022-07-14 12:35:28 +0800

    blog upgrage

--- push.sh
+++ push.sh
@@ -2,7 +2,8 @@ git add .
 git commit -m "blog upgrage"
 git push
 hugo
-docker build --platform linux/amd64 linux/arm -t mabutou/hugo-blog .
-# docker build --platform linux/amd64 -t mabutou/hugo-blog:arm .
+docker build --platform linux/amd64 -t mabutou/hugo-blog .
+docker build --platform linux/arm64 -t mabutou/hugo-blog:arm .
 docker push mabutou/hugo-blog
+docker push mabutou/hugo-blog:arm
 wrangler publish
