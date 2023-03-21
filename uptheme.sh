e0aeb6a7eb6644621b51f854e0532295cb247e57
machuang <machuang@biaoguoworks.com>
2023-03-21 17:03:45 +0800

    blog upgrage

--- uptheme.sh
+++ uptheme.sh
@@ -1,9 +1,9 @@
-cd ./themes/hugo-theme-stack
-git pull
-cp -r ../hugo-theme-stack ../stack
-cd ../stack
-rm -rf .git .github
-cd ../../
-git add .
-git commit -m "update theme"
-git push
+# cd ./themes/hugo-theme-stack
+# git pull
+# cp -r ../hugo-theme-stack ../stack
+# cd ../stack
+# rm -rf .git .github
+# cd ../../
+# git add .
+# git commit -m "update theme"
+# git push
